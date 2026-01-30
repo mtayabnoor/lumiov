0a533a7a4ada01582c1bb427d331e93f86ae072b
muno1623 <mtayabnoor@gmail.com>
2026-01-30 21:04:22 +0100

    fixed app name

--- frontend/src/components/layout/MainLayout.tsx
+++ frontend/src/components/layout/MainLayout.tsx
@@ -14,7 +14,7 @@ function MainLayout() {
       >
         <Toolbar>
           <Typography variant="h6" noWrap component="div">
-            Lumiov Test
+            Lumiov
           </Typography>
         </Toolbar>
       </AppBar>
