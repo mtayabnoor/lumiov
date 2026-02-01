181f2ecbd47436555371117ffa64b56b1714b891
muno1623 <mtayabnoor@gmail.com>
2026-02-01 18:22:53 +0100

    feat!: Frontend - Migrated to vite react

---
+++ frontend/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  base: "./", // <--- CRITICAL for Electron (makes paths relative)
+  server: {
+    port: 3000, // Keep port 3000 to match your current setup
+  },
+  build: {
+    outDir: "dist", // Output to 'dist' instead of 'dist' to match CRA
+  },
+});
