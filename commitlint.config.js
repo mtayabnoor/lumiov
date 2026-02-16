6a1bc3b489b6d27084c08e0973ed0d1125d64d0e
muno1623 <mtayabnoor@gmail.com>
2026-02-16 14:38:54 +0100

    ci: Fix sentnece case issue for dependabot

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,6 @@
 export default {
   extends: ["@commitlint/config-conventional"],
+  ignores: [(commit) => commit.includes("Signed-off-by: dependabot[bot]")],
   rules: {
     "type-enum": [
       2,
